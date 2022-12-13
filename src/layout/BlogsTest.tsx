4651240a02cbabeee66557408aa87ee45410b2dd
genuine315 <shayankalb315@gmail.com>
2022-12-13 18:02:56 +0330

    created header and footer

---
+++ src/layout/BlogsTest.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const BlogsTest = () => {
+    return (
+        <div style={{width:"65%",height:"1000px"}}>
+            <h1>blogs</h1>
+        </div>
+    );
+};
+
+export default BlogsTest;
