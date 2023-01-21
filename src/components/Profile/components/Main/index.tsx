62402e1eb07e1993409165140c5d41006af007e6
alimh17 <amohamadi17@gamil.com>
2023-01-21 22:09:21 +0330

    second merge

--- src/components/Profile/components/Main/index.tsx
+++ src/components/Profile/components/Main/index.tsx
@@ -1,4 +1,4 @@
-import React, { FC, useState } from 'react'
+import React, { FC, useEffect, useState } from 'react'
 import { useRouter } from 'next/router'
 import Link from 'next/link'
 import axios from 'axios'
@@ -7,135 +7,78 @@ import { useDispatch, useSelector } from 'react-redux'
 import { BiDotsHorizontalRounded } from 'react-icons/bi'
 import { AiOutlinePlus, AiOutlineMinus } from 'react-icons/ai'
 
-import Spinner from '../../../Spinner'
 import { getRequest } from 'api'
-import Card from 'components/Card'
 // import { useGetUserPosts } from 'hooks'
-import { accessToken } from 'store/fetchers/authSlice'
+import Spinner from '../../../Spinner'
+import Card from 'components/Card'
 import CardLoading from 'components/Loading/CardLoading'
 import Button from 'components/Button'
 
-import styles from './styles.module.scss'
-import { initProfile } from 'store/fetchers/profileSlice'
+import Head from './Head'
 import PostList from './PostList'
-interface MainProps {
-  isFollow: boolean | null
-  setFollow: () => any
-  followers: number
-  setFollowers: () => any
-}
 
-const Main: FC<MainProps> = ({
-  isFollow,
-  setFollow,
-  followers,
-  setFollowers,
-}): JSX.Element => {
-  const [options, setOptions] = useState<boolean>(false)
+import { accessToken } from 'store/fetchers/authSlice'
+import { initProfile } from 'store/fetchers/profileSlice'
+import styles from './styles.module.scss'
+interface MainProps {}
 
-  const userProfile = useSelector((state: any) => state.user.profile)
-  const user = useSelector((state: any) => state.profile?.profile?.user)
-  const userPosts = useSelector(
-    (state: any) => state.profile?.profile?.userPosts
-  )
-  const dispatch = useDispatch()
-  const access = useSelector((state: any) => state.auth.access)
-  const route = useRouter()
+const Main: FC<MainProps> = (): JSX.Element => {
+    const [isFollow, setIsFollow] = useState<any>(false)
+    const [followers, setFollowers] = useState<number>(0)
 
-  return (
-    <main className={styles.Main}>
-      <div className={styles.Head}>
-        <div>
-          {user?.profile ? (
-            <img alt="profile" className={styles.profile} src={user?.profile} />
-          ) : (
-            <div className={styles.avatar}>
-              {user?.username.slice(0, 1).toUpperCase()}
-            </div>
-          )}
-          <h1>{user?.name ? user?.name : user?.username}</h1>
-        </div>
-        <BiDotsHorizontalRounded onClick={() => setOptions(!options)} />
-        {options && (
-          <div className={styles.Options}>
-            <ul>
-              <li>لینک پروفایل</li>
-              <li>طراحی پروفایل</li>
-              {route.query.profile.slice(1) === userProfile?.username && (
-                <li>
-                  <Link href={`${route.query.profile}/setting`}>تنظیمات</Link>
-                </li>
-              )}
-            </ul>
-          </div>
-        )}
-      </div>
-      {isFollow !== null && (
-        <div className={styles.follow}>
-          <button
-            type="button"
-            onClick={() => {
-              setFollow()
-              setFollowers()
-            }}
-            style={
-              isFollow
-                ? {
-                    backgroundColor: '#fff',
-                    color: '#0081c9 ',
-                    border: '1px solid #0081c9',
-                  }
-                : { backgroundColor: '#0081c9', color: '#fff' }
-            }
-          >
-            {isFollow ? 'دنبال نکن' : 'دنبال کن'}
-            {isFollow ? <AiOutlineMinus /> : <AiOutlinePlus />}
-          </button>
-        </div>
-      )}
-      <Link href={`@${user?.username}/followers`} className={styles.followers}>
-        <span>دنبال کننده :</span>
-        <span>{followers}</span>
-      </Link>
-      <div className={styles.about}> {user?.about}</div>
-      <ul className={styles.skills}>
-        {user?.skills?.split(',').map((skill: string, i) => (
-          <li key={skill + i}>{skill}#</li>
-        ))}
-      </ul>
-      <nav className={styles.Navbar}>
-        <ul>
-          <li>خانه</li>
-          <li>
-            <Link href="/profile/about">درباره</Link>
-          </li>
-        </ul>
-      </nav>
-      <div className={styles.postList}>
-        {!userPosts && (
-          <>
-            <CardLoading />
-            <CardLoading />
-            <CardLoading />
-          </>
-        )}
-        <>
-          {userPosts?.posts.map((post: any) => (
-            <Card
-              user={post.user}
-              key={post.id}
-              title={post.title}
-              description={post.description}
-              tags={post.tags}
-              created={post.created}
-              file={post.files[0]?.file}
+    const userProfile = useSelector((state: any) => state.user.profile)
+    const user = useSelector((state: any) => state.profile?.profile?.user)
+    const userPosts = useSelector(
+        (state: any) => state.profile?.profile?.userPosts
+    )
+    const dispatch = useDispatch()
+    const access = useSelector((state: any) => state.auth.access)
+
+    useEffect(() => {
+        setIsFollow(user?.isFollowing)
+        setFollowers(user?.followers)
+    }, [])
+
+    const handleSetFollow = () => {
+        setIsFollow(!isFollow)
+    }
+
+    const handleSetFollowers = async () => {
+        const { access } = JSON.parse(
+            localStorage.getItem('medium-clone-tokens')
+        )
+        const data = {}
+
+        if (isFollow) {
+            setFollowers((current) => current - 1)
+        } else {
+            setFollowers((current) => current + 1)
+        }
+        try {
+            // const res = await followReq(`follow/${user?.id}/`, data, access)
+        } catch (err) {
+            console.log(err)
+        }
+    }
+
+    return (
+        <main className={styles.Main}>
+            <Head
+                isFollow={isFollow}
+                setFollow={handleSetFollow}
+                setFollowers={handleSetFollowers}
+                followers={followers}
             />
-          ))}
-        </>
-      </div>
-      <PostList />
-    </main>
-  )
+            <nav className={styles.Navbar}>
+                <ul>
+                    <li>خانه</li>
+                    <li>
+                        <Link href="/profile/about">درباره</Link>
+                    </li>
+                </ul>
+            </nav>
+        </main>
+    )
 }
 
 export default Main
--- src/components/Profile/index.tsx
+++ src/components/Profile/index.tsx
@@ -14,73 +14,47 @@ import Sidebar from './components/Sidebar'
 import styles from './components/Sidebar/styles.module.scss'
 
 interface ProfileProps {
-  profile: any
+    profile: any
 }
 
 const Profile: FC<ProfileProps> = ({ profile }): JSX.Element => {
-  const [isFollow, setIsFollow] = useState<boolean | null>()
-  const [followers, setFollowers] = useState<number>(0)
-  const [accessToken, setAccessToken] = useState<string>('')
+    // const [isFollow, setIsFollow] = useState<boolean | null>(null)
+    const [accessToken, setAccessToken] = useState<string>('')
 
-  // const user = useSelector((state: any) => state.profile?.profile?.user)
-  const displayForm = useSelector((state: any) => state.auth.displayForm)
-  const dispatch = useDispatch()
+    // const user = useSelector((state: any) => state.profile?.profile?.user)
+    const displayForm = useSelector((state: any) => state.auth.displayForm)
+    const dispatch = useDispatch()
 
-  useEffect(() => {
-    const getTokens = JSON.parse(localStorage.getItem('medium-clone-tokens'))
-    const { user, userPosts } = profile
-    setAccessToken(getTokens.access)
-    dispatch(access(getTokens.access))
-    dispatch(refresh(getTokens.refresh))
-    dispatch(initProfile(profile))
-    setIsFollow(user?.isFollowing)
-    setFollowers(user?.followers)
-  }, [])
+    useEffect(() => {
+        const getTokens = JSON.parse(
+            localStorage.getItem('medium-clone-tokens')
+        )
+        const { user, userPosts } = profile
+        setAccessToken(getTokens.access)
+        dispatch(access(getTokens.access))
+        dispatch(refresh(getTokens.refresh))
+        dispatch(initProfile(profile))
+    }, [])
 
-  const handleSetFollow = () => {
-    setIsFollow(!isFollow)
-  }
-
-  const handleSetFollowers = async () => {
-    const { access } = JSON.parse(localStorage.getItem('medium-clone-tokens'))
-    const data = {}
-
-    if (isFollow) {
-      setFollowers((current) => current - 1)
-    } else {
-      setFollowers((current) => current + 1)
-    }
-    try {
-      // const res = await followReq(`follow/${user?.id}/`, data, access)
-    } catch (err) {
-      console.log(err)
-    }
-  }
-
-  return (
-    <div
-      style={
-        displayForm
-          ? { height: '100vh', overflow: 'hidden', display: 'flex' }
-          : { display: 'flex' }
-      }
-    >
-      {displayForm && (
-        <Modal displayForm={displayForm} setDisplayForm={toggle}>
-          <Auth />
-        </Modal>
-      )}
-      <Main
-        isFollow={isFollow}
-        setFollow={handleSetFollow}
-        followers={followers}
-        setFollowers={handleSetFollowers}
-      />
-      <div className={styles.container}>
-        <Sidebar />
-      </div>
-    </div>
-  )
+    return (
+        <div
+            style={
+                displayForm
+                    ? { height: '100vh', overflow: 'hidden', display: 'flex' }
+                    : { display: 'flex' }
+            }
+        >
+            {displayForm && (
+                <Modal displayForm={displayForm} setDisplayForm={toggle}>
+                    <Auth />
+                </Modal>
+            )}
+            <Main />
+            <div className={styles.container}>
+                <Sidebar />
+            </div>
+        </div>
+    )
 }
 
 export default Profile
